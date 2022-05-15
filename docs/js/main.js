a202f2e9036215adbb43eabbaa957ad64fb617a9
Ivan <ivan.kalunuch12@gmail.com>
2022-05-15 16:00:51 +0300

    lkjsaf

--- docs/js/main.js
+++ docs/js/main.js
@@ -15,7 +15,7 @@ const anchors = document.querySelectorAll('a[href*="#"]')
 for (let anchor of anchors) {
   anchor.addEventListener('click', function (e) {
     e.preventDefault()
-    
+
     const blockID = anchor.getAttribute('href').substr(1);
     console.log(blockID)
     document.getElementById(blockID).scrollIntoView({
@@ -24,3 +24,28 @@ for (let anchor of anchors) {
     })
   })
 }
+
+const orderButtons = document.querySelectorAll('.order');
+const modalWindow = document.querySelector('.modalForms');
+const exit = document.querySelector('.exit');
+const form = document.querySelector('.form')
+orderButtons.forEach(element=>{
+    element.addEventListener('click',()=>{
+        modalWindow.classList.add('modalForms--active')
+        body.classList.add('body--active');
+    })
+})
+
+const closeModal = ()=>{
+    modalWindow.classList.remove('modalForms--active')
+    body.classList.remove('body--active');
+}
+
+exit.addEventListener('click',()=>{
+    closeModal();
+})
+modalWindow.addEventListener('click',(e)=>{
+    if(e.target === modalWindow){
+        closeModal()
+    }
+})
